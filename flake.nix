ce39ffc432f2e59292e062213ca215af940fc908
DCsunset <DCsunset@protonmail.com>
2026-05-11 21:42:57 +0000

    chore(flake): remove flake-parts

--- flake.nix
+++ flake.nix
@@ -4,49 +4,53 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
+  outputs = inputs: let
+    inherit (inputs.nixpkgs) lib;
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f:
+      lib.genAttrs
+        systems
+        (system: f (
+          import inputs.nixpkgs ({
+            inherit system;
+          })
+        ));
+  in {
+    devShells = forAllSystems (pkgs: let
+      deps = with pkgs; [
+        (pandoc-include.overrideAttrs (_: {
+          src = ./.;
+          name = "pandoc-include-master";
+        }))
+        (python3.withPackages (ps: with ps; [
+          panflute
+          natsort
+          lxml
+          build
+        ]))
       ];
-
-      perSystem = { self', system, pkgs, ... }: let
-        deps = with pkgs; [
-          (pandoc-include.overrideAttrs (_: {
-            src = ./.;
-            name = "pandoc-include-master";
-          }))
-          (python3.withPackages (ps: with ps; [
-            panflute
-            natsort
-            lxml
-            build
-          ]))
-        ];
-      in {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = deps;
-          };
-        };
-
-        apps = {
-          release = {
-            type = "app";
-            program = pkgs.writeShellScriptBin "release" ''
-              set -e
-
-              ver=$(git cliff --bumped-version)
-              ver=''${ver:1}
-
-              sed -i "s/^version = \".*\"/version = \"$ver\"/" pyproject.toml
-              git cliff --bump -o CHANGELOG.md
-              git add -A
-              git commit -m "chore(release): v$ver"
-              git tag "v$ver"
-            '';
-          };
-        };
+    in {
+      default = pkgs.mkShell {
+        packages = deps;
       };
-    };
+    });
+
+    apps = forAllSystems (pkgs: {
+      release = {
+        type = "app";
+        program = lib.getExe (pkgs.writeShellScriptBin "release" ''
+          set -e
+
+          ver=$(git cliff --bumped-version)
+          ver=''${ver:1}
+
+          sed -i "s/^version = \".*\"/version = \"$ver\"/" pyproject.toml
+          git cliff --bump -o CHANGELOG.md
+          git add -A
+          git commit -m "chore(release): v$ver"
+          git tag "v$ver"
+        '');
+      };
+    });
+  };
 }
